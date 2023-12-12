bd94e42d05995238fde374359c59aa16e92e5177
AlberteVallentin <alberte@vallentin.eu>
2023-12-12 21:28:24 +0100

    Create Account

--- TODOAPP/src/main/java/com/example/fxtesting/CreateAccountController.java
+++ TODOAPP/src/main/java/com/example/fxtesting/CreateAccountController.java
@@ -1,8 +1,13 @@
 package com.example.fxtesting;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 
@@ -10,6 +15,43 @@ public class CreateAccountController {
 
     @FXML
     private AnchorPane createAccountAchor;
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField userNameField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private CheckBox showPasswordBtn;
+
+    @FXML
+    private AnchorPane accountCreated;
+
+
+    @FXML
+    void loginFromCreatedAccount(ActionEvent event) throws IOException {
+        new SceneSwitch(accountCreated,"login.fxml");
+    }
+
+    @FXML
+    void createAccount(ActionEvent event) throws IOException {
+        try (FileWriter fileWriter = new FileWriter("/Users/albertevallentin/Documents/GitHub/FinalICERepository/TODOAPP/src/Database/Users", true)) {
+            String userCredentials = userNameField.getText() + ";" + passwordField.getText() + System.lineSeparator();
+            fileWriter.write(userCredentials);
+        } catch (IOException e) {
+            e.printStackTrace(); // Log the exception or handle it appropriately
+        }
+
+        new SceneSwitch(createAccountAchor,"signupSucces.fxml");
+    }
+
 
     @FXML
     void goBackClicked(ActionEvent event) throws IOException {
